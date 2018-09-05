d3d8e0fc5e25405968f240beeaa63e7fc9fbda88
Iman Jawad <imanj12@gmail.com>
2018-09-05 17:37:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,11 @@
 
 def fizzbuzz(int)
   if (int % 15 == 0)
-    return "Fizzbuzz"
+    return "FizzBuzz"
   elsif (int % 5 == 0)
     return "Buzz"
-  
+  elsif (int % 3 == 0)
+    return "Fizz"
+  else
+    
 end
